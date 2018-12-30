c170b710d2638b1074f4db0a29369a12ca4f85c9
Michael Winata <32146559+mwinata94@users.noreply.github.com>
2018-12-30 14:03:52 +0800

    finish homepage + group nav

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onLogin(event) {
+    let apiCheck = true;
+    if (apiCheck) {
+      window.location = '/menu';
+    }
+  }
+
   render() {
-    return <h1>Homepage</h1>;
+    return (
+      <div className="hero is-fullheight">
+        <div className="hero-body">
+          <div className="tile is-vertical is-ancestor">
+            <div className="tile is-parent">
+              <div className="tile is-child">
+                <input
+                  className="input is-large is-expanded"
+                  name="username"
+                  onChange={this.onChange}
+                  type="text"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="tile is-child">
+                <input
+                  className="input is-large is-expanded"
+                  name="password"
+                  onChange={this.onChange}
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child">
+                <div
+                  className="button is-info is-large is-fullwidth"
+                  onClick={this.onLogin}
+                >
+                  Login
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Containers/Menu/Master/Group/Group.js
+++ src/Containers/Menu/Master/Group/Group.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import NavigationBar from '../../../../Generics/NavigationBar';
 
 class MasterGroup extends Component {
   render() {
-    return <h1>MasterGroup</h1>;
+    return NavigationBar([
+      {
+        name: 'create',
+        align: 'left',
+        onClick: event => {},
+        child: []
+      },
+      {
+        name: 'edit',
+        align: 'left',
+        onClick: event => {},
+        child: []
+      },
+      {
+        name: 'delete',
+        align: 'left',
+        onClick: event => {},
+        child: []
+      },
+      {
+        name: 'back',
+        align: 'right',
+        onClick: event => {
+          window.location = '../../menu';
+        },
+        child: []
+      }
+    ]);
   }
 }
 
