9cff514dc123bd1991f6ad59d4290c7ec446a12a
Michael Winata <32146559+mwinata94@users.noreply.github.com>
2018-12-31 17:35:31 +0800

    refactor navbar tables into states

--- src/Containers/Menu/Master/Group/Group.js
+++ src/Containers/Menu/Master/Group/Group.js
@@ -2,39 +2,42 @@ import React, { Component } from 'react';
 import NavigationBar from '../../../../Generics/NavigationBar/NavigationBar';
 
 class MasterGroup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigationBarTable: [
+        {
+          name: 'create',
+          align: 'left',
+          onClick: event => {},
+          child: []
+        },
+        {
+          name: 'edit',
+          align: 'left',
+          onClick: event => {},
+          child: []
+        },
+        {
+          name: 'delete',
+          align: 'left',
+          onClick: event => {},
+          child: []
+        },
+        {
+          name: 'back',
+          align: 'right',
+          onClick: event => {
+            window.location = '../../menu';
+          },
+          child: []
+        }
+      ]
+    };
+  }
+
   render() {
-    return (
-      <NavigationBar
-        table={[
-          {
-            name: 'create',
-            align: 'left',
-            onClick: event => {},
-            child: []
-          },
-          {
-            name: 'edit',
-            align: 'left',
-            onClick: event => {},
-            child: []
-          },
-          {
-            name: 'delete',
-            align: 'left',
-            onClick: event => {},
-            child: []
-          },
-          {
-            name: 'back',
-            align: 'right',
-            onClick: event => {
-              window.location = '../../menu';
-            },
-            child: []
-          }
-        ]}
-      />
-    );
+    return <NavigationBar table={this.state.navigationBarTable} />;
   }
 }
 
--- src/Containers/Menu/Menu.js
+++ src/Containers/Menu/Menu.js
@@ -2,140 +2,145 @@ import React, { Component } from 'react';
 import NavigationBar from '../../Generics/NavigationBar/NavigationBar';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigationBarTable: [
+        {
+          name: 'master',
+          align: 'left',
+          onClick: event => {},
+          child: [
+            {
+              name: 'group',
+              onClick: event => {
+                window.location = './menu/master/group';
+              }
+            },
+            {
+              name: 'inventory',
+              onClick: event => {
+                window.location = './menu/master/inventory';
+              }
+            },
+            {
+              name: 'location',
+              onClick: event => {
+                window.location = './menu/master/location';
+              }
+            },
+            {
+              name: 'mechanic',
+              onClick: event => {
+                window.location = './menu/master/mechanic';
+              }
+            },
+            {
+              name: 'quality',
+              onClick: event => {
+                window.location = './menu/master/quality';
+              }
+            },
+            {
+              name: 'retail sale',
+              onClick: event => {
+                window.location = './menu/master/retailsale';
+              }
+            },
+            {
+              name: 'supplier',
+              onClick: event => {
+                window.location = './menu/master/supplier';
+              }
+            }
+          ]
+        },
+        {
+          name: 'transaction',
+          align: 'left',
+          onClick: event => {},
+          child: [
+            {
+              name: 'adjust',
+              onClick: event => {
+                window.location = './menu/transaction/adjust';
+              }
+            },
+            {
+              name: 'purchase',
+              onClick: event => {
+                window.location = './menu/transaction/purchase';
+              }
+            },
+            {
+              name: 'sale',
+              onClick: event => {
+                window.location = './menu/transaction/sale';
+              }
+            }
+          ]
+        },
+        {
+          name: 'report',
+          align: 'left',
+          onClick: event => {},
+          child: [
+            {
+              name: 'adjust',
+              onClick: event => {
+                window.location = './menu/report/adjust';
+              }
+            },
+            {
+              name: 'inventory',
+              onClick: event => {
+                window.location = './menu/report/inventory';
+              }
+            },
+            {
+              name: 'purchase',
+              onClick: event => {
+                window.location = './menu/report/purchase';
+              }
+            },
+            {
+              name: 'sale',
+              onClick: event => {
+                window.location = './menu/report/sale';
+              }
+            }
+          ]
+        },
+        {
+          name: 'wish list',
+          align: 'right',
+          onClick: event => {
+            window.location = './menu/wishlist';
+          },
+          child: []
+        },
+        {
+          name: 'options',
+          align: 'right',
+          onClick: event => {},
+          child: [
+            {
+              name: 'logout',
+              onClick: event => {
+                // to do backend api
+              }
+            }
+          ]
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="hero is-fullheight">
         <div className="hero-head">
-          <NavigationBar
-            table={[
-              {
-                name: 'master',
-                align: 'left',
-                onClick: event => {},
-                child: [
-                  {
-                    name: 'group',
-                    onClick: event => {
-                      window.location = './menu/master/group';
-                    }
-                  },
-                  {
-                    name: 'inventory',
-                    onClick: event => {
-                      window.location = './menu/master/inventory';
-                    }
-                  },
-                  {
-                    name: 'location',
-                    onClick: event => {
-                      window.location = './menu/master/location';
-                    }
-                  },
-                  {
-                    name: 'mechanic',
-                    onClick: event => {
-                      window.location = './menu/master/mechanic';
-                    }
-                  },
-                  {
-                    name: 'quality',
-                    onClick: event => {
-                      window.location = './menu/master/quality';
-                    }
-                  },
-                  {
-                    name: 'retail sale',
-                    onClick: event => {
-                      window.location = './menu/master/retailsale';
-                    }
-                  },
-                  {
-                    name: 'supplier',
-                    onClick: event => {
-                      window.location = './menu/master/supplier';
-                    }
-                  }
-                ]
-              },
-              {
-                name: 'transaction',
-                align: 'left',
-                onClick: event => {},
-                child: [
-                  {
-                    name: 'adjust',
-                    onClick: event => {
-                      window.location = './menu/transaction/adjust';
-                    }
-                  },
-                  {
-                    name: 'purchase',
-                    onClick: event => {
-                      window.location = './menu/transaction/purchase';
-                    }
-                  },
-                  {
-                    name: 'sale',
-                    onClick: event => {
-                      window.location = './menu/transaction/sale';
-                    }
-                  }
-                ]
-              },
-              {
-                name: 'report',
-                align: 'left',
-                onClick: event => {},
-                child: [
-                  {
-                    name: 'adjust',
-                    onClick: event => {
-                      window.location = './menu/report/adjust';
-                    }
-                  },
-                  {
-                    name: 'inventory',
-                    onClick: event => {
-                      window.location = './menu/report/inventory';
-                    }
-                  },
-                  {
-                    name: 'purchase',
-                    onClick: event => {
-                      window.location = './menu/report/purchase';
-                    }
-                  },
-                  {
-                    name: 'sale',
-                    onClick: event => {
-                      window.location = './menu/report/sale';
-                    }
-                  }
-                ]
-              },
-              {
-                name: 'wish list',
-                align: 'right',
-                onClick: event => {
-                  window.location = './menu/wishlist';
-                },
-                child: []
-              },
-              {
-                name: 'options',
-                align: 'right',
-                onClick: event => {},
-                child: [
-                  {
-                    name: 'logout',
-                    onClick: event => {
-                      // to do backend api
-                    }
-                  }
-                ]
-              }
-            ]}
-          />
+          <NavigationBar table={this.state.navigationBarTable} />
         </div>
       </div>
     );
