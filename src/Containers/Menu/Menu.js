2a1ced29236f8c6bcb0f702ad4e17dae20e1ecf7
Michael Winata <32146559+mwinata94@users.noreply.github.com>
2019-01-04 07:32:06 +0700

    add printer status to main menu

--- src/Containers/Menu/Menu.js
+++ src/Containers/Menu/Menu.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import MyHero from '../../Generics/MyHero/MyHero';
+import DisplayTable from '../../Generics/DisplayTable/DisplayTable';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // TODO
+    // api call to fetch printer tables
+    this.setState({
+      defaultTable: {
+        columns: ['printer name', 'status'],
+        rows: [
+          {
+            columns: ['Printer', 'not connected'],
+            onClick: _ => {},
+            onMouseEnter: _ => {}
+          }
+        ]
+      }
+    });
+  }
+
   render() {
-    return <MyHero navTable="menu" heroBody={<div />} />;
+    return (
+      <MyHero
+        navTable="menu"
+        heroBody={<DisplayTable table={this.state.defaultTable} />}
+      />
+    );
   }
 }
 
