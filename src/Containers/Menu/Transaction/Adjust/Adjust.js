8cc7217e56a1c5084cd34582d10feaecbb4965c8
Michael Winata <32146559+mwinata94@users.noreply.github.com>
2019-01-04 14:29:11 +0700

    progress on TransactionAdjust

--- src/Containers/Menu/Transaction/Adjust/Adjust.js
+++ src/Containers/Menu/Transaction/Adjust/Adjust.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import MyHero from '../../../../Generics/MyHero/MyHero';
+import DisplayTable from '../../../../Generics/DisplayTable/DisplayTable';
 
 class TransactionAdjust extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      footer: ' '
+    };
+  }
+
+  componentDidMount() {
+    // TODO
+    // api call to fetch adjust tables
+    // onClick edit
+    this.setState({
+      defaultTable: {
+        columns: ['id', 'date'],
+        rows: [
+          {
+            columns: ['QWE', 'Qwerty'],
+            onClick: _ => (window.location = '/menu/transaction/adjust/QWE'),
+            onMouseEnter: _ => this.setState({ footer: 'QWE Qwerty' })
+          }
+        ]
+      }
+    });
+  }
+
   render() {
-    return <h1>TransactionAdjust</h1>;
+    return (
+      <MyHero
+        navTable="transactionAdjust"
+        heroBody={<DisplayTable table={this.state.defaultTable} />}
+        alignTop={true}
+        footer={this.state.footer}
+      />
+    );
   }
 }
 
