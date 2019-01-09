e37e8a601b28f04d67faa4794d9ed990a44cdc08
Michael Winata <mwinata@mail.sfsu.edu>
2019-01-09 18:23:09 +0700

    finish ReportTable layout + data format

--- src/Containers/Menu/Report/Adjust/Report.js
+++ src/Containers/Menu/Report/Adjust/Report.js
@@ -7,7 +7,7 @@ class ReportAdjustReport extends Component {
       columns: ['id', 'name'],
       rows: [
         {
-          columns: ['testId', 'testName'],
+          columns: ['testId1', 'testName1'],
           data: {
             columns: ['id', 'name', 'qty'],
             rows: [
@@ -19,6 +19,29 @@ class ReportAdjustReport extends Component {
               },
               {
                 columns: ['2', 'test2', 1],
+                onMouseEnter: _ => {
+                  console.log(2);
+                }
+              }
+            ]
+          },
+          onMouseEnter: _ => {
+            console.log(0);
+          }
+        },
+        {
+          columns: ['testId2', 'testName2'],
+          data: {
+            columns: ['id', 'name', 'qty'],
+            rows: [
+              {
+                columns: ['1', 'test1', 9],
+                onMouseEnter: _ => {
+                  console.log(1);
+                }
+              },
+              {
+                columns: ['2', 'test2', 4],
                 onMouseEnter: _ => {
                   console.log(2);
                 }
--- src/Generics/ReportTable/ReportTable.js
+++ src/Generics/ReportTable/ReportTable.js
@@ -6,35 +6,96 @@ class ReportTable extends Component {
     this.dropdownOnClick = this.dropdownOnClick.bind(this);
   }
 
-  dropdownOnClick = _ => {
-    document.querySelector('.dropdown').classList.toggle('is-active');
+  dropdownOnClick = event => {
+    document
+      .getElementsByClassName('dropdown')
+      [event.target.getAttribute('name')].classList.toggle('is-active');
   };
 
   render() {
     if (this.props.table) {
       if (this.props.table.rows.length) {
         return (
-          <div className="dropdown">
-            <div className="dropdown-trigger">
-              <button
-                className="button"
-                aria-haspopup="true"
-                aria-controls="dropdown-menu"
-                onClick={this.dropdownOnClick}
-              >
-                <span>Dropdown button</span>
-              </button>
-            </div>
-            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-              <div className="dropdown-content">
-                <a className="dropdown-item">Dropdown item</a>
-                <a className="dropdown-item">Other dropdown item</a>
-                <a className="dropdown-item">Active dropdown item</a>
-                <a className="dropdown-item">Other dropdown item</a>
-                <a className="dropdown-item">With a divider</a>
-              </div>
-            </div>
-          </div>
+          <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                {this.props.table.columns.map((e, i) => (
+                  <th key={i} className="has-text-centered">
+                    {e}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.table.rows.map((e, i) => (
+                <tr
+                  name={i}
+                  key={i}
+                  onClick={this.dropdownOnClick}
+                  onMouseEnter={e.onMouseEnter}
+                >
+                  {e.columns.map((e, j) => (
+                    <td key={j} name={i}>
+                      {e}
+                    </td>
+                  ))}
+                  <div
+                    className="dropdown"
+                    name={i}
+                    style={{
+                      left: '0%',
+                      paddingTop: '2em',
+                      position: 'absolute',
+                      width: '100vw'
+                    }}
+                  >
+                    <div
+                      className="dropdown-menu"
+                      id="dropdown-menu"
+                      role="menu"
+                      style={{
+                        width: '95vw',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        position: 'relative'
+                      }}
+                    >
+                      <div className="dropdown-content">
+                        {
+                          <div className="dropdown-item">
+                            <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                              <thead>
+                                <tr>
+                                  {e.data.columns.map((e, i) => (
+                                    <th key={i} className="has-text-centered">
+                                      {e}
+                                    </th>
+                                  ))}
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {e.data.rows.map((e, i) => (
+                                  <tr
+                                    key={i}
+                                    onClick={e.onClick}
+                                    onMouseEnter={e.onMouseEnter}
+                                  >
+                                    {e.columns.map((e, i) => (
+                                      <td key={i}>{e}</td>
+                                    ))}
+                                  </tr>
+                                ))}
+                              </tbody>
+                            </table>
+                          </div>
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         );
       } else {
         return <div className="is-size-3">No Data Found</div>;
